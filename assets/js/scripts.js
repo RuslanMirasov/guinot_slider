ecd7e894eb7f580deac0a9ce6195c5746e6c0876
Mirasov <info@mirasov.dev>
2024-07-02 00:01:40 +0200

    desctop

---
+++ assets/js/scripts.js
@@ -0,0 +1,9 @@
+const discontSlider = new Swiper('.discont-slider', {
+  loop: true,
+  speed: 1000,
+  slidesPerView: 1,
+  pagination: {
+    el: '.discont-swiper-pagination',
+    clickable: true,
+  },
+});
