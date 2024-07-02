9ae8634565efc83eaae6d9a51417d892cb87d94a
Mirasov <info@mirasov.dev>
2024-07-02 03:14:33 +0200

    mobil

--- assets/js/scripts.js
+++ assets/js/scripts.js
@@ -2,6 +2,11 @@ const discontSlider = new Swiper('.discont-slider', {
   loop: true,
   speed: 1000,
   slidesPerView: 1,
+  autoHeight: true,
+  autoplay: {
+    delay: 3000,
+    disableOnInteraction: false,
+  },
   pagination: {
     el: '.discont-swiper-pagination',
     clickable: true,
